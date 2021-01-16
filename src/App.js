bc47e26c753060d06f13b61ccd20abb2f061aeb2
Wenszel <wiktor.smaga13@gmail.com>
2021-01-16 19:09:02 +0100

    http token request added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import {Credentials} from './Credentials'
+import axios from 'axios'
+const App = () => {
+  const spotify = Credentials();
+  const [token, setToken] = useState('');
+  useEffect(()=>{
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {      
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+    });
+  },[]);
+  
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+  return(
+    <div></div>
+  );
+  
 }
-
 export default App;
---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '[Spotify Client ID]',
+        ClientSecret: '[Spotify Client Secret]'
+    }
+}
+
+export { Credentials };
