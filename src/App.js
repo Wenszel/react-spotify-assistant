b11d4bfad992291d7889491595611cd56e95e8e4
Wenszel <wiktor.smaga13@gmail.com>
2021-01-18 14:57:32 +0100

    spotifylogin component unmount after login added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Credentials} from './Credentials'
 import axios from 'axios'
 import './index.css'
 const App = (props) => {
-  const [userToken, setUserToken] = useState('');
+  const [userToken, setUserToken] = useState(props.token);
   const [songsList, setSongsList] = useState([]);
-  const getLastestSongs = ()=>{
+  useEffect(()=>{
+  const LastestSongs = ()=>{
     console.log(props.token)
     axios('https://api.spotify.com/v1/me/player/recently-played?limit=10', {
       headers:{
@@ -16,13 +17,15 @@ const App = (props) => {
       method: 'GET'
     }).then(listResponse => {
       setSongsList(listResponse.data.items);
-        return <ul>{songsList.map(element => <li>{element.track.track}</li>)}</ul>
     })
+    return <ul>{songsList.map(element => <li>{element.track.track}</li>)}</ul>
   }
+})
   return(
     <div>
+      <p>{userToken}</p>
         <h1>Witaj użytkowniku</h1>
-        <button onClick={getLastestSongs}>Get songs</button>
+        <button>Get songs</button>
     </div>
   );
   
--- src/Login.js
+++ src/Login.js
@@ -5,25 +5,39 @@ import App from './App'
 import SpotifyLogin from 'react-spotify-login';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const Login = () => {
+  const [isLogged, setIsLogged] = useState(false);
   const [userToken, setUserToken] = useState('');
-  const onSuccess = response => setUserToken(response.access_token);
+  let token;
+  const onSuccess = async response => {
+    token = response.access_token
+  handleUnmount()
+  }
   const onFailure = response => console.error(response);
   const spotify = Credentials();
+  const handleUnmount=()=>{
+    setIsLogged(true)
+  }
   return(
+      
     <Router>
         <Link to="/logged">
+        {isLogged ? null:
             <SpotifyLogin 
+            
             className="login-button"
             clientId={spotify.ClientId}
             scope={'user-read-recently-played','user-top-read'}
             redirectUri={spotify.redirectUri}
             onSuccess={onSuccess}
             onFailure={onFailure}/>
+          }
         </Link>
         <Route path="/logged" >
-            <App token = {userToken}/>
+        
+            <App token = {userToken} />
         </Route>
     </Router>
+  
   );
 }
 export default Login;
