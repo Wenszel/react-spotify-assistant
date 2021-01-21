8ac313358dae0ccdf14fe52a4e2c4bfb69c02eb8
Wenszel <wiktor.smaga13@gmail.com>
2021-01-21 17:38:27 +0100

    username and userimage fetching added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import './index.css'
 const App = (props) => {
+  const [userToken] = useState(props.token)
   const [songsList, setSongsList] = useState([]);
+  const [userImage, setUserImage] = useState('');
+  const [username, setUsername] = useState('');
+  useEffect(()=>{
+    axios('https://api.spotify.com/v1/me', {
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer '+ userToken,
+      },
+      method: 'GET'
+    }).then(listResponse => {
+      setUsername(listResponse.data.display_name);
+      setUserImage(listResponse.data.images[0].url);
+    })
+  },[])
   const getSongs = ()=>{
     axios('https://api.spotify.com/v1/me/player/recently-played?limit=10', {
       headers:{
         'Accept': 'application/json',
         'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer '+ props.token,
+        'Authorization' : 'Bearer '+ userToken,
       },
       method: 'GET'
     }).then(listResponse => {
@@ -20,7 +36,8 @@ const App = (props) => {
   }
   return(
     <div>
-        <h1>Witaj użytkowniku</h1>
+        <h1>Hello {username}</h1>
+        <img src={userImage} alt="Profile"/>
         <button onClick={handleGetSongsClick}>Get songs</button>
         <ul>
         {songsList.map(item=><li>{item.track.name}</li>)}
