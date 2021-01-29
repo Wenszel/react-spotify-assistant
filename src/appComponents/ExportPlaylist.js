3538306c4153f30bb04c1e9d78e49a27c5044f0f
Wenszel <wiktor.smaga13@gmail.com>
2021-01-29 11:51:53 +0100

    export playlist edited
    edited playlist name and added alert

--- src/appComponents/ExportPlaylist.js
+++ src/appComponents/ExportPlaylist.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const ExportPlaylist = ({uris,userId,token})=>{
     const createPlaylist = (()=>{
         axios.post(`https://api.spotify.com/v1/users/${userId}/playlists`,{
-            name: "Playlist "+new Date(),
+            name: "Playlist "+new Date().toString().slice(0,24),
             public: false
           },{
             headers:{
@@ -22,7 +22,7 @@ const ExportPlaylist = ({uris,userId,token})=>{
                 'Content-Type' : 'application/json',
                 'Authorization' : 'Bearer '+ token
               }
-        })
+        }).then(alert("Playlist exported"));
     }
     return <button className="download-button" onClick={createPlaylist}>Export Playlist</button>
 }
